b9f42046ccf72a6739251622f2a57091aac820b1
André Camargo <andrefelipe_camargo@hotmail.com>
2022-11-24 21:52:56 -0300

    new

---
+++ uri-challenges/uri1008.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+                Locale.setDefault(Locale.US);
+                Scanner sc = new Scanner(System.in);
+
+                int numero = sc.nextInt();
+                int horas = sc.nextInt();
+                double valorHora = sc.nextDouble();
+
+                double salario = valorHora * horas;
+
+                System.out.println("NUMBER = " + numero);
+                System.out.printf("SALARY = U$ %.2f%n", salario);
+
+
+
+        }
+
+}
