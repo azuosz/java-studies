431e8946a17e956b34aa438eb558ca6c71e0dba9
André Camargo <andrefelipe_camargo@hotmail.com>
2022-11-24 21:27:48 -0300

    Create uri1006.java

---
+++ uri-challenges/uri1006.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Main {
+
+        public static void main(String[] args) {
+
+                Locale.setDefault(Locale.US);
+                Scanner sc = new Scanner(System.in);
+
+                double a, b, c, media;
+
+                a = sc.nextDouble() * 2.0;
+                b = sc.nextDouble() * 3.0;
+                c = sc.nextDouble() * 5.0;
+
+                media = (a + b + c) / 10.0;
+
+                System.out.printf("MEDIA = %.1f%n", media);
+
+                sc.close();
+        }
+}
